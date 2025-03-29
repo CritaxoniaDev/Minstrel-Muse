f9e063d263960cb8ee7f9b333aa1c955cbeac188
Gian Alcantara <raphaelalcantara51@gmail.com>
2025-03-29 16:34:58 +0800

    added obfuscated styles

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,23 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? {
+      'postcss-rename': {
+        strategy: 'minimal', // or 'hex' or 'crypto'
+        prefix: '_', // optional prefix for generated class names
+        // Exclude certain patterns from renaming
+        exclude: [
+          /^(html|body)$/,
+          // Add any other selectors you don't want to rename
+        ]
+      },
+      cssnano: {
+        preset: ['default', {
+          discardComments: {
+            removeAll: true,
+          },
+        }],
+      }
+    } : {})
   },
 }
