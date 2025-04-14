5e7137cb2d6ee04c2a9ffeef7335013430f439c7
Gian Alcantara <raphaelalcantara51@gmail.com>
2025-04-14 22:13:20 +0800

    implemented android app

--- src/main.jsx
+++ src/main.jsx
@@ -7,19 +7,6 @@ import '@fontsource/public-sans'
 // Add CSS import
 import './index.css'
 
-const updateSW = registerSW({
-  onNeedRefresh() {
-    // You can show a notification to the user that there's an update
-    if (confirm('New content available. Reload?')) {
-      updateSW(true)
-    }
-  },
-  onOfflineReady() {
-    console.log('App ready to work offline')
-    // You can show a notification that the app is ready for offline use
-  }
-})
-
 // Register service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
