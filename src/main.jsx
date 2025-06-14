32e869a181270913ced49bc2059e3911d5d82567
Gian Alcantara <raphaelalcantara51@gmail.com>
2025-06-14 20:40:49 +0800

    updated font style

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import App from './App.jsx'
-import '@fontsource/public-sans'
 
 // Add CSS import
 import './index.css'
